f80972ca303aa3636b03981cc45f4b21d730b15a
Mohammed Ismayil <mohammedismayil10@gmail.com>
2022-05-15 21:26:26 +0530

    deployed a hugsender contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,11 +5,12 @@ const main = async () => {
   console.log("Deploying contracts with account: ", deployer.address);
   console.log("Account balance: ", accountBalance.toString());
 
-  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContractFactory = await hre.ethers.getContractFactory("SendHugs");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
 
-  console.log("WavePortal address: ", waveContract.address);
+  // deployed contract address --0x3146c4B1893a964BCA6a05152D826c81dEd47408
+  console.log("HugSender address: ", waveContract.address);
 };
 
 const runMain = async () => {
